b55341348d3e5912c208100a483153ea2afb4665
Galang  Satrio Wicaksono <galang0559@gmail.com>
2023-05-11 21:32:00 +0700

    Mei_11_2131
    buat class calculator

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -2,8 +2,31 @@
 
 using namespace std;
 
+class Calculator {
+    private:
+        float* numbers;
+    public:
+        float* getNumbers() {
+            return numbers;
+        }
+        void setNumbers(float* numbers, int size) {
+            this->numbers = new float[size];
+            for (int i = 0; i < size; i++) {
+                this->numbers[i] = numbers[i];
+            }
+        }
+        void printResult(int size) {
+            cout << "Result: ";
+            float* resultArray = getNumbers();
+            cout << resultArray[size - 1] << endl;
+        }
+        void clearResult() {
+        delete[] numbers;
+        numbers = nullptr;
+        }
+
+};
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+
 }
