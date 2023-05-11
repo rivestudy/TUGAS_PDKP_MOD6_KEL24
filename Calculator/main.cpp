ecb415bf63aa749c73ccf7ae4e5fff5a3ebda734
Galang  Satrio Wicaksono <galang0559@gmail.com>
2023-05-11 21:47:40 +0700

    Mei_11_2147
    buat fungsi lain (sub -, multiply x, divide /) uji di main class

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -54,9 +54,101 @@ class Addition : public Calculator{
         clearResult();
     }
 };
+class Subtract : public Calculator{
+    public :
+    void performSubtraction(int size) {
+            float* numbers = getNumbers();
+            for (int i = 1; i < size; i++) {
+                numbers[i] = numbers[i-1] - numbers[i];
+            }
+        }
+    void subtract(){
+        int n = 0;
+        float numbers[100];
+        while (n < 100) {
+            cout << "Enter number #" << (n+1) << ": ";
+            string input;
+            cin >> input;
+            if (input == "x") {
+                break;
+            }
+            float num = stof(input);
+            numbers[n] = num;
+            n++;
+        }
+        setNumbers(numbers, n);
+        performSubtraction(n);
+        printResult(n);
+        clearResult();
+    }
+};
+
+class Multiply : public Calculator{
+    public :
+    void performMultiply(int size) {
+            float* numbers = getNumbers();
+            for (int i = 1; i < size; i++) {
+                numbers[i] = numbers[i-1] * numbers[i];
+            }
+        }
+    void multiply(){
+        int n = 0;
+        float numbers[100];
+        while (n < 100) {
+            cout << "Enter number #" << (n+1) << ": ";
+            string input;
+            cin >> input;
+            if (input == "x") {
+                break;
+            }
+            float num = stof(input);
+            numbers[n] = num;
+            n++;
+        }
+        setNumbers(numbers, n);
+        performMultiply(n);
+        printResult(n);
+        clearResult();
+    }
+};
+
+class Divide : public Calculator{
+    public :
+    void performDivide(int size) {
+            float* numbers = getNumbers();
+            for (int i = 1; i < size; i++) {
+                numbers[i] = numbers[i-1] / numbers[i];
+            }
+        }
+    void divide(){
+        int n = 0;
+        float numbers[100];
+        while (n < 100) {
+            cout << "Enter number #" << (n+1) << ": ";
+            string input;
+            cin >> input;
+            if (input == "x") {
+                break;
+            }
+            float num = stof(input);
+            numbers[n] = num;
+            n++;
+        }
+        setNumbers(numbers, n);
+        performDivide(n);
+        printResult(n);
+        clearResult();
+    }
+};
 
 int main()
 {
     Addition add;
     add.addition();
+    Subtract sub;
+    sub.subtract();
+    Multiply mul;
+    mul.multiply();
+    Divide div;
+    div.divide();
 }
